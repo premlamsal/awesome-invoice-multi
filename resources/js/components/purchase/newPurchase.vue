6e2f8072eb63d5e12df8d18183d58fbc45d4629f
premlamsal2@gmail.com <premlamsal2@gmail.com>
2020-10-27 17:44:40 +0545

    changes on price while adding new purchase

--- resources/js/components/purchase/newPurchase.vue
+++ resources/js/components/purchase/newPurchase.vue
@@ -135,7 +135,7 @@
                       <!--  suggestion block -->
                       <div class="product-search-suggestion-purchase" v-for="queryResultsProduct in queryResultsProducts[index]" v-bind:key="queryResultsProduct.id">
                         <ul>
-                          <li @click="clickSearchProductSuggestion(queryResultsProduct.id,queryResultsProduct.custom_product_id,queryResultsProduct.name,queryResultsProduct.unit.id,queryResultsProduct.price,index)">{{queryResultsProduct.name}} -></li>
+                          <li @click="clickSearchProductSuggestion(queryResultsProduct.id,queryResultsProduct.custom_product_id,queryResultsProduct.name,queryResultsProduct.unit.id,queryResultsProduct.cp,index)">{{queryResultsProduct.name}} -></li>
                         </ul>
                       </div>
                       <!--  <span v-if="errors['items.' + index + '.product_name']">
@@ -488,7 +488,7 @@ export default {
       custom_product_id,
       product_name,
       unit_id,
-      price,
+      cp,
       index
     ) {
 
@@ -504,7 +504,7 @@ export default {
         Vue.set(this.items[index], "unit_id", unit_id);
 
         Vue.set(
-          this.items[index], "price" , parseFloat(price)
+          this.items[index], "price" , parseFloat(cp)
         );
 
 
@@ -519,8 +519,7 @@ export default {
         Vue.set(
           this.cloneItems[index],
           "price",
-          parseFloat(price) +
-          (parseFloat(price) * parseFloat(this.store.profit_percentage)) / 100
+          parseFloat(cp) 
         );
 
         // this.items[index] = this.items[index] + (this.store.profit_percentage)/100;
